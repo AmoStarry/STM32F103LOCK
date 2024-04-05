7382728e0ef0716cba9d991298c0c0736c233a6f
AmoStarry <1524730261@qq.com>
2024-04-05 19:07:43 +0800

    编写完字符串数组

--- HARDWARE/esp8266.c
+++ HARDWARE/esp8266.c
@@ -2,6 +2,15 @@
 #include <Serial.h>
 #include <Delay.h>
 
+char AT_RESTORE[] =     {"AT\r\n"};
+char AT_CWMODE[] =      {"AT+CWMODE=1\r\n"};
+char AT_CIPSNTPCFG[] =  {"AT+CIPSNTPCFG=1,8,\"ntp1.aliyun.com\"\r\n"};
+char AT_CWJAP[] =       {"AT+CWJAP=\"22\",\"11111111\"\r\n"};
+char AT_MQTTUSERCFG[] = {"AT+MQTTUSERCFG=0,1,\"NULL\",\"esp8266&k0ygtxfPbcQ\",\"6f3afe7e1b5c81adca541957bb9774bbe1958c7b85c07c2737b0b13a75834e45\",0,0,\"\"\r\n"};
+char AT_MQTTCLIENTID[] ={"AT+MQTTCLIENTID=0,\"k0ygtxfPbcQ.esp8266|securemode=2\\,signmethod=hmacsha256\\,timestamp=1712023569066|\"\r\n"};
+char AT_MQTTCONN[] =    {"AT+MQTTCONN=0,\"iot-06z00edcq1sto7u.mqtt.iothub.aliyuncs.com\",1883,1\r\n"};
+char AT_MQTTSUB[] =     {"AT+MQTTSUB=0,\"/sys/k0ygtxfPbcQ/esp8266/thing/service/property/set\",1\r\n"};    
+
 void esp8266_init(void)
 {
      printf("AT\r\n");
@@ -24,4 +33,6 @@ void esp8266_init(void)
 //     Delay_ms(5000);
 //     printf("AT+MQTTPUB=0,\"/sys/k0ygtxfPbcQ/esp8266/thing/event/property/post\",\"{\\\"params\\\":{\\\"temp\\\":88\\,\\\"humi\\\":66\\,\\\"led\\\":1}\\,\\\"version\\\":\\\"1.0.0\\\"}\",1,0\r\n");
 //     Delay_ms(5000);
+     
+     Serial_SendString(AT_RESTORE);
 }
