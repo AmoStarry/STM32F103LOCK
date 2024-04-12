7cdebb73cd6b60d0ede6e2ecccb767762750327f
AmoStarry <1524730261@qq.com>
2024-04-12 21:20:17 +0800

    3.0丝滑的界面切换编写成功，下一步加二级菜单

--- HARDWARE/Menu.c
+++ HARDWARE/Menu.c
@@ -15,24 +15,24 @@ void Show_Menu_Config(void)
      
 }
 
-////参数1：移动的起始位置（左定点为基准） 参数2：方向（1左 -1右） 参数3：移动范围
-//void animateDisplay(int8_t initialDisplay, int8_t direction, int8_t maxDisplay) {
-//    int8_t display = initialDisplay;
-//    while ((direction > 0 && display < maxDisplay) || (direction < 0 && display > -maxDisplay)) {
-//        u8g2_ClearBuffer(&u8g2); // 清除显示缓存
-//        u8g2_DrawXBM(&u8g2, 0 + direction * display, 16, 32, 32, password);
-//        u8g2_DrawBox(&u8g2, 32 + direction * display, 0, 64, 64);
-//        u8g2_DrawXBM(&u8g2, 32 + direction * display, 0, 64, 64, Image[i]);
-//        u8g2_DrawXBM(&u8g2, 96 + direction * display, 16, 32, 32, setting);
-//        u8g2_SendBuffer(&u8g2); // 把要显示的信息写入缓存
-//        display += 3;           // 更新位置
-//    }
-//}	
+//参数1：移动的起始位置（左定点为基准） 参数2：方向（1左 -1右） 参数3：移动范围
+void animateDisplay(int8_t direction, int8_t maxDisplay) {
+    int8_t display =0;
+    while ((direction > 0 && display < maxDisplay) || (direction < 0 && display > -maxDisplay)) {
+        u8g2_ClearBuffer(&u8g2); // 清除显示缓存
+        u8g2_DrawXBM(&u8g2,  0 + display, 16, 32, 32, password);
+        u8g2_DrawBox(&u8g2, 32 + display, 0, 64, 64);
+        u8g2_DrawXBM(&u8g2, 32 + display, 0, 64, 64, Image[i]);
+        u8g2_DrawXBM(&u8g2, 96 + display, 16, 32, 32, setting);
+        u8g2_SendBuffer(&u8g2); // 把要显示的信息写入缓存
+        display += direction * 3; // 更新位置
+    }
+    u8g2_ClearBuffer(&u8g2); // 清除显示缓存
+}	
 
 
 void Show_Menu(Speed_ENUM Speed_choose)        // 显示菜单函数，参数为动态显示速度的枚举类型
-{
-     
+{   
     uint8_t index = 1; // 当前菜单索引，从0开始 
     uint8_t display =0;
     uint8_t key;
@@ -43,27 +43,31 @@ void Show_Menu(Speed_ENUM Speed_choose)        // 显示菜单函数，参数为动态显示速
                       
         if (key == 1)       // 检查按键，根据按键更新索引
         {   
-            display =0;
-            index = (index == 0) ? 1 : index - 1;
+          if (index == 0) {
+              index = 1;
+              animateDisplay(-1, 32); // 当 index 为 0 时执行这个函数
+          } 
+          else {
+              index = index - 1;
+              animateDisplay( 1, 32);  // 当 index 不为 0 时执行这个函数
+          }
+
         } 
         else if (key == 2) 
         {
-            display =0;
-            index = (index == 2) ? 1 : index + 1;
+          if (index == 2) {
+              index = 1;
+              animateDisplay(1, 32); // 当 index 不为 0 时执行这个函数              
+          } 
+          else {
+              index = index + 1;          //右移
+              animateDisplay(-1, 32); // 当 index 为 0 时执行这个函数
+          }
         }    
          
         switch(index)
         {
              case 0:
-               for(;display <32;display+=3)
-               {
-                    u8g2_ClearBuffer(&u8g2);                        // 清除显示缓存
-                    u8g2_DrawXBM(&u8g2, 0+display,16,32,32,password); 
-                    u8g2_DrawBox(&u8g2,32+display, 0, 64, 64);
-                    u8g2_DrawXBM(&u8g2,32+display, 0, 64, 64, Image[i]);
-                    u8g2_DrawXBM(&u8g2,96+display,16,32,32,setting);
-                    u8g2_SendBuffer(&u8g2);                         //把要显示的信息写入缓存                    
-               };
                u8g2_ClearBuffer(&u8g2);                        // 清除显示缓存
                u8g2_DrawXBM(&u8g2,32,0,64,64,password_big);    //传入设置图标图片
                u8g2_SendBuffer(&u8g2);                         //把要显示的信息写入缓存
@@ -80,15 +84,6 @@ void Show_Menu(Speed_ENUM Speed_choose)        // 显示菜单函数，参数为动态显示速
                Delay_ms(10); // 延时10ms          
                break;
           case 2:
-               for(;display <32;display+=3)
-               {
-                    u8g2_ClearBuffer(&u8g2);                        // 清除显示缓存
-                    u8g2_DrawXBM(&u8g2, 0-display,16,32,32,password);
-                    u8g2_DrawBox(&u8g2,32-display, 0, 64, 64);
-                    u8g2_DrawXBM(&u8g2,32-display, 0, 64, 64, Image[i]);
-                    u8g2_DrawXBM(&u8g2,96-display,16,32,32,setting);
-                    u8g2_SendBuffer(&u8g2);                         //把要显示的信息写入缓存
-               }
                u8g2_ClearBuffer(&u8g2);                        // 清除显示缓存
                u8g2_DrawXBM(&u8g2,32,0,64,64,setup);           //传入设置图标图片
                u8g2_SendBuffer(&u8g2);                         //把要显示的信息写入缓存
